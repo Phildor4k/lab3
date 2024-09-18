9a6d2f28213442b2d908ac69ffbb800ee3d3fa30
Phildor4k <s.abdujabborov@newuu.uz>
2024-09-18 12:19:09 +0500

    init

---
+++ main.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+
+using namespace std;
+//void checkNum(int a); //funxction delclaration
+//
+//// Problem 1
+//int main() {
+//    int x;
+//    cout<< "Enter the x:\n>>>";
+//    cin>>x;
+//    checkNum(x);
+//   return 0;
+//}
+//
+//void checkNum(int a){ //function definition
+//    if (a>=0){
+//        cout<< "X is a positive number.";
+//    }else{
+//        cout<<"X is a negative number.";
+//    }
+//}
+// Problem 2
+//int main(){
+//    int x;
+//    cout<< "Enter the x:\n>>>";
+//    cin>>x;
+//    if (x>=0){
+//        if(x%2==0){
+//            cout<< "X is a positive and even number.";
+//        }
+//        else{
+//            cout<< "X is a positive and odd number.";
+//        }
+//    }else{
+//        if(x%2==0){
+//            cout<<"X is a negative and even number.";
+//        }else{
+//            cout<<"X is a negative and odd number.";
+//        }
+//    }
+//}
+
+//Problem 3
+//int main(){
+//    int x;
+//    cout<< "Enter the x:\n>>>";
+//    cin>>x;
+//    int y;
+//    cout<< "Enter the y:\n>>>";
+//    cin>>y;
+//    int z;
+//    cout<< "Enter the z:\n>>>";
+//    cin>>z;
+//    if (x>y && x>z){
+//        cout<<"x is the largest number.";
+//    }
+//    else if(y>x && y>z){
+//        cout<<"y is the largest number.";
+//    }
+//    else if(z>x && z>y){
+//        cout<<"z is the largest number.";
+//    }
+//}
+
+//Problem 4
+
+//int main(){
+//    int x;
+//    cout<< "Enter the x:\n>>>";
+//    cin>>x;
+//    int y;
+//    cout<< "Enter the y:\n>>>";
+//    cin>>y;
+//    if (x%y==0){
+//        cout<<x<<" is divisible by "<<y;
+//    }else{
+//        cout<<x<<" is not divisible by "<<y;
+//    }
+//}
+
+// Problem 5
+
+//int main(){
+//    int year;
+//    cout<< "Enter the year:\n>>>";
+//    cin>>year;
+//    if (year%4==0 && year%100==0 && year%400==0){
+//        cout<<"This is the leap year";
+//    }else{
+//        cout<<"This is not the leap year";
+//    }
+//}
+
+//Problem 6
+#include <cmath>
+//int main(){
+//    float a;
+//    cout<< "Enter the a:\n>>>";
+//    cin>>a;
+//    float b;
+//    cout<< "Enter the b:\n>>>";
+//    cin>>b;
+//    float c;
+//    cout<< "Enter the c:\n>>>";
+//    cin>>c;
+//    float d = pow(b,2)-4*a*c;
+//    float root_d = pow(d,(1.0/2));
+//    cout<<(-b-root_d)/(2*a)<<" This is the first solution to the equation";
+//    cout<<(-b+root_d)/(2*a)<<" This is the second solution to the equation";
+//}
+
+//Problem 7
+//
+//int main(){
+//    float a;
+//    cout<< "Enter the a:\n>>>";
+//    cin>>a;
+//    float b;
+//    cout<< "Enter the b:\n>>>";
+//    cin>>b;
+//    float c;
+//    cout<< "Enter the c:\n>>>";
+//    cin>>c;
+//    if(a+b+c==180){
+//        cout<<"This triangle is valid";
+//    }else{
+//        cout<<"This triangle is invalid";
+//    }
+//}
+
+//Problem 9
+
+//int main(){
+//    float a;
+//    cout<< "Enter the a:\n>>>";
+//    cin>>a;
+//    float b;
+//    cout<< "Enter the b:\n>>>";
+//    cin>>b;
+//    float root =  pow((a*a)+(b*b),2);
+//    if(pow(root,1/2.0)<=100) {
+//        cout << "Point is in the circle.";
+//    }else{
+//        cout<<"Point is not in the circle";
+//    }
+//}
+
+//Problem 10
+
+//int main(){
+//    float a;
+//    cout<< "Enter the value of the first product:\n>>>";
+//    cin>>a;
+//    float b;
+//    cout<< "Enter the :\n>>>";
+//    cin>>b;
+//    float c;
+//    cout<< "Enter the c:\n>>>";
+//    cin>>c;
+//    float d;
+//    cout<< "Enter the d:\n>>>";
+//    cin>>d;
+//    if ((a*b)<=(c*d)){
+//        cout<< "First package is better.";
+//    }else{
+//        cout<<"Second package is better";
+//    }
+//}
+
+
+//problem 8
+//int main(){
+//    char a;
+//    cin>> a;
+//    if(a>='a' && a<='z'){
+//        cout<<"Lowercase";
+//    }else if (a>='A' && a<='Z'){
+//        cout<<"Uppercase";
+//    }else{
+//        cout<<"Not in alphabet";
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
